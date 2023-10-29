744566c3c2f0aeec18ea33b52baa0180c6575e16
shipilov_maxim <skam3840@gmail.com>
2023-10-29 21:19:46 +0400

    add SJAPI

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 from abc import abstractmethod, ABC
-import requests
 from datetime import datetime
+import os
+import requests
 
 
 class APIVacancy(ABC):
 
     @abstractmethod
-    def get_vacancies(self, vacancy, salary):
+    def get_vacancies(self, vacancy):
         pass
 
     @abstractmethod
@@ -18,49 +19,48 @@ class HeadHunterAPI(APIVacancy):
     def __init__(self):
         self.url = 'https://api.hh.ru/vacancies'
 
-    def get_vacancies(self, vacancy, salary):
+    def get_vacancies(self, vacancy):
         headers = {'User-Agent': 'skam3840@gmail.com'}
         parameters = {'text': vacancy,
                       'only_with_salary': True,
-                      'salary': salary,
                       'per_page': 100}
-
         response = requests.get(self.url, headers=headers, params=parameters).json()
         return response
 
     def format_data(self, data):
         vacancy_dict = {}
         for vacancy in data['items']:
-            vacancy_id = vacancy['id']
-            if vacancy['salary']['from'] is not None:
-                salary_from = f"от {vacancy['salary']['from']} "
-            else:
-                salary_from = ""
-            if vacancy['salary']['to'] is not None:
-                salary_to = f"до {vacancy['salary']['to']} "
-            else:
-                salary_to = ""
-            requirement = vacancy['snippet']['requirement']
-            responsibility = vacancy['snippet']['responsibility']
-            published = vacancy['published_at']
-            date_published = datetime.strptime(published, "%Y-%m-%dT%H:%M:%S%z")
-            vacancy_dict[vacancy_id] = {'url': f"https://hh.ru/vacancy/{vacancy_id}",
-                                        'name': vacancy['name'],
-                                        'salary': f"{salary_from}{salary_to}{vacancy['salary']['currency']}",
-                                        'requirement': requirement,
-                                        'responsibility': responsibility,
-                                        'date_published': date_published.strftime("%d.%m.%Y")}
-
+            date_published = datetime.strptime(vacancy['published_at'], "%Y-%m-%dT%H:%M:%S%z")
+            vacancy_dict[vacancy['id']] = {'url': f"https://hh.ru/vacancy/{vacancy['id']}",
+                                           'name': vacancy['name'],
+                                           'salary': [vacancy['salary']['from'], vacancy['salary']['to'],
+                                                      vacancy['salary']['currency']],
+                                           'requirement': vacancy['snippet']['requirement'],
+                                           'responsibility': vacancy['snippet']['responsibility'],
+                                           'date_published': date_published.strftime("%d.%m.%Y")}
         return vacancy_dict
 
 
-class SuperJobAPI:
-    pass
+class SuperJobAPI(APIVacancy):
+    def __init__(self):
+        self.url = 'https://api.superjob.ru/2.0/vacancies/'
 
     def get_vacancies(self, vacancy):
-        pass
+        api_key: str = os.getenv('SUPERJOB_API_KEY')
+        headers = {f'Host': 'api.superjob.ru',
+                   'X-Api-App-Id': api_key,
+                   'Authorization': 'Bearer r.000000010000001.example.access_token',
+                   'Content-Type': 'application/x-www-form-urlencoded'}
+        parameters = {'keyword': vacancy,
+                      # 'payment_value': salary,
+                      'payment_defined': 1,
+                      'per_page': 100}
 
-    def format_data(self):
+        response = requests.get(self.url, headers=headers, params=parameters).json()
+
+        return response
+
+    def format_data(self, data):
         pass
 
 
@@ -79,6 +79,6 @@ class JSONSaver:
         pass
 
 
-hh_api = HeadHunterAPI()
-hh_vacancies = hh_api.get_vacancies("Python", 10000)
-print(hh_api.format_data(hh_vacancies))
+hh_api = SuperJobAPI()
+hh_vacancies = hh_api.get_vacancies("Python")
+print(hh_vacancies)
