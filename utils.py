ae9d5f15bf54d5f21d1e10bd6300e92a6e8aa63b
shipilov_maxim <skam3840@gmail.com>
2023-10-29 14:24:38 +0400

    Initial commit

---
+++ utils.py
@@ -0,0 +1,29 @@
+class HeadHunterAPI:
+    pass
+
+    def get_vacancies(self):
+        pass
+
+
+class Vacancy:
+    pass
+
+
+class SuperJobAPI:
+    pass
+
+    def get_vacancies(self):
+        pass
+
+
+class JSONSaver:
+    def add_vacancy(self):
+        pass
+
+    def get_vacancies_by_salary(self):
+        pass
+
+    def delete_vacancy(self):
+        pass
+
+
